9e87763fb4a1161e1997be0af7be650f157d01ee
vishnukkrishna <vishnukkrishna1999@gmail.com>
2023-06-15 11:21:52 +0530

    Complete Terms and condition and Submit button

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tkinter
 from tkinter import ttk
 
 
+def enter_data():
+    print("Hi")
+
 
 window = tkinter.Tk()
 window.title("Data Entry Form")
@@ -43,7 +46,7 @@ for widget in user_info_frame.winfo_children():
 
 # Saving Course Information
 courses_frame = tkinter.LabelFrame(frame)
-courses_frame.grid(row=1, column=0, sticky="news", padx=20, pady=20)
+courses_frame.grid(row=1, column=0, sticky="news", padx=20, pady=10)
 
 registered_label = tkinter.Label(courses_frame, text="Registration Status")
 registered_check = tkinter.Checkbutton(courses_frame, text="Currently Registered")
@@ -60,12 +63,18 @@ numsemesters_spinbox = tkinter.Spinbox(courses_frame, from_=0, to='infinity')
 numsemesters_label.grid(row=0, column=2)
 numsemesters_spinbox.grid(row=1, column=2)
 
-for widget in user_info_frame.winfo_children():
+for widget in courses_frame.winfo_children():
     widget.grid_configure(padx=10, pady=5)
 
 # Accept terms
-# terms_frame = tkinter.LabelFrame(frame, text="Terms & Conditions.")
-# terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=20)
+terms_frame = tkinter.LabelFrame(frame, text="Terms & Conditions.")
+terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=10)
 
+terms_check = tkinter.Checkbutton(terms_frame, text="I accept the terms and conditions")
+terms_check.grid(row=0, column=0)
+
+# Button
+button = tkinter.Button(frame, text="Enter Data", command=enter_data)
+button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
 
 window.mainloop()
